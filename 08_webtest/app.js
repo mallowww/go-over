bd3010d193d004755d164510576506e06ceb473b
mell <bu_chanon@kkumail.com>
2022-11-24 08:47:21 +0700

    :white_check_mark: test(cors): restapi

--- 08_webtest/app.js
+++ 08_webtest/app.js
@@ -1,10 +1,18 @@
-// const baseURL = 'https://api.sampleapis.com/coffee/hot';
-  const baseURL = 'https://c269-2001-fb1-16a-470-5da5-6978-f8b3-a919.ap.ngrok.io/course';
+const baseURL = 'https://85e6-2001-fb1-16a-470-5da5-6978-f8b3-a919.ap.ngrok.io/course';
 fetch(baseURL)
 .then(resp => resp.json())
 .then(data => appendData(data))
-.catch(err => console.log(`error: `+ err));
-
+.catch(err => console.log(`error: ${err}`));
+/*
+.then(function (response) {
+  return response.json();
+}).then(function (data) {
+  appendData(data)
+}).catch(function (err) {
+  console.log(`error ${err}`)
+});
+*/
+console.log(data[0].ID)
 function appendData(data) {
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
