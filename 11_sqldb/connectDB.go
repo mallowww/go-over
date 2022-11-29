ee9abcea2f630dd6cfee14519cfebc4e88bc058a
mell <bu_chanon@kkumail.com>
2022-11-29 22:24:06 +0700

    :sparkles: feat(db): add insert, delete records

--- 11_sqldb/connectDB.go
+++ 11_sqldb/connectDB.go
@@ -4,9 +4,41 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func createTable(db *sql.DB) {
+	query := `CREATE TABLE users (
+		id INT AUTO_INCREMENT,
+		username TEXT NOT NULL,
+		password TEXT NOT NULL,
+		created_at DATETIME,
+		PRIMARY KEY (id)
+		);`
+
+	if _, err := db.Exec(query); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Insert(db *sql.DB) {
+	var (
+		username string
+		password string
+	)
+	fmt.Scan(&username)
+	fmt.Scan(&password)
+	createdAt := time.Now()
+
+	res, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?,?,?)`, username, password, createdAt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	id, err := res.LastInsertId()
+	fmt.Println(id)
+}
 
 func query(db *sql.DB) {
 	var (
@@ -26,6 +58,15 @@ func query(db *sql.DB) {
 	fmt.Println(id, coursename, price, instructor)
 }
 
+func delete(db *sql.DB) {
+	var deleteID int
+	fmt.Scan(&deleteID)
+	_, err := db.Exec(`DELETE FROM users WHERE id = ?`, deleteID)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	db, err := sql.Open("mysql", "root:mysqlbro124@tcp(127.0.0.1:3306)/coursedb")
 	if err != nil {
@@ -33,6 +74,7 @@ func main() {
 	} else {
 		fmt.Println("connect successfully")
 	}
-	// fmt.Println(db)
-	query(db)
+	// createTable(db)
+	// Insert(db)
+	delete(db)
 }
