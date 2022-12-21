5a4024b17c02c143ad012a975c2436d4e05d6bee
mell <chanon4817@gmail.com>
2022-12-22 03:07:40 +0700

    :construction: test(api): integration test - wip

---
+++ 21_echoapi/server_integration_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestGetAllUser(t *testing.T) {
+	// insert ของ
+	var c User
+	body := bytes.NewBufferString(`{
+		"name": "letstryit",
+		"age": 14
+	}`)
+	err := request(http.MethodPost, uri("users"), body).Decode(&c)
+	if err != nil {
+		t.Fatal("can't create users - ", err)
+	}
+
+	// ยิง request มาดูของที่ได้ว่า success รึไม่
+	var us []User
+	res := request(http.MethodGet, uri("users"), nil)
+	err = res.Decode(&us)
+
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusOK, res.StatusCode)
+	assert.Greater(t, len(us), 0)
+
+}
+
+func uri(paths ...string) string {
+	host := "http://localhost:8080"
+	if paths == nil {
+		return host
+	}
+
+	url := append([]string{host}, paths...)
+	return strings.Join(url, "/")
+}
+
+type Response struct {
+	*http.Response
+	err error
+}
+
+func (r *Response) Decode(v interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func request(method, url string, body io.Reader) *Response {
+	req, _ := http.NewRequest(method, url, body)
+	req.Header.Add("Authorization", os.Getenv("AUTH_TOKEN"))
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	res, err := client.Do(req)
+	return &Response{res, err}
+}
